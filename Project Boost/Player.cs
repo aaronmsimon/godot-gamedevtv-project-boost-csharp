f6868b9f56931ae954211d030a1e17c181c78f98
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2024-04-01 23:41:07 -0700

    The Export Annotation (10_PB_G3D)

--- Project Boost/Player.cs
+++ Project Boost/Player.cs
@@ -2,6 +2,9 @@ using Godot;
 
 public partial class Player : RigidBody3D
 {
+	[Export(PropertyHint.Range,"750,3000")] private float thrust = 1000f;
+	[Export] private float torque = 100f;
+
     public override void _Ready()
     {
         BodyEntered += OnBodyEntered;
@@ -11,15 +14,15 @@ public partial class Player : RigidBody3D
 	{
 		if (Input.IsActionPressed("boost"))
 		{
-			ApplyCentralForce(Basis.Y * (float)delta * 1000f);
+			ApplyCentralForce(Basis.Y * (float)delta * thrust);
 		}
 		if (Input.IsActionPressed("rotate_left"))
 		{
-			ApplyTorque(Vector3.Back * (float)delta * 100f);
+			ApplyTorque(Vector3.Back * (float)delta * torque);
 		}
 		if (Input.IsActionPressed("rotate_right"))
 		{
-			ApplyTorque(Vector3.Forward * (float)delta * 100f);
+			ApplyTorque(Vector3.Forward * (float)delta * torque);
 		}
 	}
 
