464d752e23c3033c79ea156e78eb89c850f806a2
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2024-04-01 23:04:21 -0700

    Collision Detection (9_PB_G3D)

--- Project Boost/Player.cs
+++ Project Boost/Player.cs
@@ -1,9 +1,13 @@
-using System;
 using Godot;
 
 public partial class Player : RigidBody3D
 {
-	public override void _Process(double delta)
+    public override void _Ready()
+    {
+        BodyEntered += OnBodyEntered;
+    }
+
+    public override void _Process(double delta)
 	{
 		if (Input.IsActionPressed("boost"))
 		{
@@ -16,6 +20,18 @@ public partial class Player : RigidBody3D
 		if (Input.IsActionPressed("rotate_right"))
 		{
 			ApplyTorque(Vector3.Forward * (float)delta * 100f);
+		}
+	}
+
+	private void OnBodyEntered(Node body)
+	{
+		if (body.GetGroups().Contains("Goal"))
+		{
+			GD.Print("You win!");
+		}
+		if (body.GetGroups().Contains("Hazard"))
+		{
+			GD.Print("You missed the goal.");
 		}
 	}
 }
