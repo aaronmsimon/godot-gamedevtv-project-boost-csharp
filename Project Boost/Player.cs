96cec5f20f1d6e320924c91b0e57a4aec4806207
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2024-04-02 20:09:58 -0700

    Controlling Audio With Scripts (16_PB_G3D)

--- Project Boost/Player.cs
+++ Project Boost/Player.cs
@@ -10,6 +10,7 @@ public partial class Player : RigidBody3D
 
 	private AudioStreamPlayer explosionAudio;
 	private AudioStreamPlayer successAudio;
+	private AudioStreamPlayer3D rocketAudio;
 
 	private bool isTransitioning = false;
 
@@ -17,6 +18,7 @@ public partial class Player : RigidBody3D
     {
 		explosionAudio = GetNode<AudioStreamPlayer>("ExplosionAudio");
 		successAudio = GetNode<AudioStreamPlayer>("SuccessAudio");
+		rocketAudio = GetNode<AudioStreamPlayer3D>("RocketAudio");
 
         BodyEntered += OnBodyEntered;
     }
@@ -26,11 +28,22 @@ public partial class Player : RigidBody3D
 		if (Input.IsActionPressed("boost"))
 		{
 			ApplyCentralForce(Basis.Y * (float)delta * thrust);
+			if (!rocketAudio.Playing)
+			{
+				rocketAudio.Play();
+			}
 		}
+
+		if (Input.IsActionJustReleased("boost"))
+		{
+			rocketAudio.Stop();
+		}
+
 		if (Input.IsActionPressed("rotate_left"))
 		{
 			ApplyTorque(Vector3.Back * (float)delta * torque);
 		}
+
 		if (Input.IsActionPressed("rotate_right"))
 		{
 			ApplyTorque(Vector3.Forward * (float)delta * torque);
